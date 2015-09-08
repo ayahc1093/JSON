11e4cd800213a58b9d9e14814c8a3c860e5174e8
schreiby <ettie.schreiber@gmail.com>
2015-09-08 00:38:18 -0400

    added .trim() to Strings that are incorporated in urlString

---
+++ app/src/main/java/com/candeapps/json/Text.java
@@ -0,0 +1,22 @@
+package com.candeapps.json;
+
+/**
+ * Created by Ettie on 9/7/2015.
+ */
+public class Text {
+
+    private String he;
+    private String eng;
+
+    public Text() {
+    //no-arg constructor
+    }
+
+    public Text(String he, String eng) {
+        this.he = he;
+        this.eng = eng;
+    }
+
+
+
+}
